020caedb185832af1233a0e0831670f796c43139
Zero <billymmartin03@gmail.com>
2020-03-14 01:35:22 -0400

    Small code update

--- lua/autorun/server/sv_zerm.lua
+++ lua/autorun/server/sv_zerm.lua
@@ -1,7 +1,7 @@
 zerm_ts = {};
 zerm_is = {};
 
-local files, directory = file.Find("sound/tttmusic/traitors/*.mp3", "GAME")
+local files, directory = file.Find("sound/tttmusic/traitors/*.mp3", "GAME");
 for i = 1, #files do
 	files[i] = "tttmusic/traitors/" .. files[i];
 	table.insert(zerm_ts, files[i]);
@@ -9,7 +9,7 @@ for i = 1, #files do
 	util.PrecacheSound(files[i]);
 end
 
-local files, directory = file.Find("sound/tttmusic/innocent/*.mp3", "GAME")
+local files, directory = file.Find("sound/tttmusic/innocent/*.mp3", "GAME");
 for i = 1, #files do
 	files[i] = "tttmusic/innocent/" .. files[i];
 	table.insert(zerm_is, files[i]);
@@ -19,7 +19,7 @@ end
 
 util.AddNetworkString("zerm_play");
 hook.Add("TTTEndRound", "SoundManager", function (winType)
-	if winType == WIN_INNOCENT then
+	if (winType == WIN_INNOCENT) then
 		local max = table.Count(zerm_is);
 		local s2p = math.Round(math.Rand(1, max));
 		net.Start("zerm_play");
