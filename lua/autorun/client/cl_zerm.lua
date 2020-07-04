a4a2f66a75cb9913a7704a5cea1af4cddea2861b
Zero <billymmartin03@gmail.com>
2020-07-04 17:22:00 -0400

    Addon fix
    Fixed broken code and added a console command.

--- lua/autorun/client/cl_zerm.lua
+++ lua/autorun/client/cl_zerm.lua
@@ -1,4 +1,22 @@
 net.Receive("zerm_play", function ()
-	local s2p = net.ReadString();
-	surface.PlaySound(s2p);
+	local s2p = tostring(net.ReadString());
+	print("Sound Selected: " .. s2p);
+	surface.PlaySound(Sound(s2p));
+end);
+
+net.Receive("zerm_transfer", function ()
+	local str1 = net.ReadString();
+	local tbl1 = net.ReadTable();
+	local str2 = net.ReadString();
+	local tbl2 = net.ReadTable();
+	print(str1);
+	PrintTable(tbl1, 1);
+	print(str2);
+	PrintTable(tbl2, 1);
+	chat.AddText(Color(25, 230, 22), "Table data received! Check your console!");
+end);
+
+concommand.Add("printSounds", function (ply, cmd, args)
+	net.Start("zerm_transfer");
+	net.SendToServer();
 end);
--- lua/autorun/server/sv_zerm.lua
+++ lua/autorun/server/sv_zerm.lua
@@ -5,7 +5,7 @@ local files, directory = file.Find("sound/tttmusic/traitors/*.mp3", "GAME");
 for i = 1, #files do
 	files[i] = "tttmusic/traitors/" .. files[i];
 	table.insert(zerm_ts, files[i]);
-	resource.AddFile(files[i]);
+	resource.AddFile("sound/" .. files[i]);
 	util.PrecacheSound(files[i]);
 end
 
@@ -13,7 +13,7 @@ local files, directory = file.Find("sound/tttmusic/innocent/*.mp3", "GAME");
 for i = 1, #files do
 	files[i] = "tttmusic/innocent/" .. files[i];
 	table.insert(zerm_is, files[i]);
-	resource.AddFile(files[i]);
+	resource.AddFile("sound/" .. files[i]);
 	util.PrecacheSound(files[i]);
 end
 
@@ -32,4 +32,28 @@ hook.Add("TTTEndRound", "SoundManager", function (winType)
 			net.WriteString(zerm_ts[s2p]);
 		net.Broadcast();
 	end
+end);
+
+local function check(ply)
+	if (IsValid(ply) && ply:IsSuperAdmin()) then
+		net.Start("zerm_transfer");
+			net.WriteString("Traitors Sound Table:");
+			net.WriteTable(zerm_ts);
+			net.WriteString("Innocents Sound Table:");
+			net.WriteTable(zerm_is);
+		net.Send(ply);
+		return;
+	elseif (IsValid(ply) && !ply:IsSuperAdmin()) then
+		ply:ChatPrint("[ERROR]: This command is superadmin / console only.");
+		return;
+	end
+end
+util.AddNetworkString("zerm_transfer");
+net.Receive("zerm_transfer", function (_, ply)
+	check(ply);
+end);
+concommand.Add("printSounds", function (ply, cmd, args)
+	check(ply);
+	PrintTable(zerm_ts, 4);
+	PrintTable(zerm_is, 4);
 end);
